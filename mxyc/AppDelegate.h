d070a468000451c01d91eb0cb0aab0ee8d153f3c
lic <lic@lichodeMac-mini.local>
2016-02-23 16:54:36 +0800

    first use this git

---
+++ mxyc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mxyc
+//
+//  Created by lic on 16/2/23.
+//  Copyright © 2016年 lic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mxyc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mxyc
+//
+//  Created by lic on 16/2/23.
+//  Copyright © 2016年 lic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
